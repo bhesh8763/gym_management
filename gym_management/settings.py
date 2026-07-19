e2a8931aa3d70822dbf38986c83940288b9a1d90
RupeshPradhan369 <140105702+RupeshPradhan369@users.noreply.github.com>
2026-07-19 20:45:08 +0545

    Add frontend for attendance, payments, staff, lockers, equipment with shared theme

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -150,5 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:8080',
     'http://127.0.0.1:8080',
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
 ]
 CORS_ALLOW_CREDENTIALS = True
